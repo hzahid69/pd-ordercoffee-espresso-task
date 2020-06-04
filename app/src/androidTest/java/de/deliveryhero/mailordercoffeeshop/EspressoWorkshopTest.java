6e2c893f606e9ff66cc4b004a862cf118644e081
hzahid69 <35507276+hzahid69@users.noreply.github.com>
2020-06-05 03:34:19 +0500

    Changes

--- app/src/androidTest/java/de/deliveryhero/mailordercoffeeshop/EspressoWorkshopTest.java
+++ app/src/androidTest/java/de/deliveryhero/mailordercoffeeshop/EspressoWorkshopTest.java
@@ -16,35 +16,28 @@ import org.junit.Test;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.doubleClick;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-//@Large
-//@RunWith(AndroidJUnit4.class)
 public class EspressoWorkshopTest {
 
-    private String stringToBetyped;
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule
             = new ActivityTestRule<>(MainActivity.class);
 
-//    @Before
-//    public void initValidString() {
-//        // Specify a valid string.
-//        stringToBetyped = "Espresso";
-//    }
+
 
     @Test
     public void CloseOnboardingscreen() {
         // Type text and then press the button.
         onView(withId(R.id.close_button)).perform(click());
+        onView(withId(R.id.more_espresso)).perform(click());
     }
-    @Test
-    public void PlaceandOrder(){
-     //onView(withId(R.id.more_espresso)).perform(doubleClick());
-     //onView(withid(R.id.more_espresso)).perform(click());
-     onView(withId(R.id.chocolate)).perform(click());
-     onData(withId(R.id.milk_options_container)).atPosition(3).perform(click());
-    }
+//    @Test
+//    public void PlaceandOrder(){
+//     //onView(withId(R.id.more_espresso)).perform(doubleClick());
+//     //onView(withid(R.id.more_espresso)).perform(click());
+//     onView(withId(R.id.chocolate)).perform(click());
+//     onData(withId(R.id.milk_options_container)).atPosition(3).perform(click());
+//    }
 }
