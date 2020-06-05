ce0aaf5e4c101d5a2f8f8e18b966d4c693ab8d82
hzahid69 <35507276+hzahid69@users.noreply.github.com>
2020-06-05 13:41:45 +0500

    Changes

--- app/src/androidTest/java/de/deliveryhero/mailordercoffeeshop/EspressoWorkshopTest.java
+++ app/src/androidTest/java/de/deliveryhero/mailordercoffeeshop/EspressoWorkshopTest.java
@@ -1,6 +1,10 @@
 
 package de.deliveryhero.mailordercoffeeshop;
 
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 
 import org.junit.Assert;
@@ -15,14 +19,20 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.addGlobalAssertion;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.CursorMatchers.withRowString;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 public class EspressoWorkshopTest  {
 
@@ -34,17 +44,20 @@ public class EspressoWorkshopTest  {
 
 
     @Test
-    public void CloseOnboardingscreen() {
+    public void TestScript() {
         onView(withId(R.id.close_button)).perform(click());
         onView(withId(R.id.more_espresso)).perform(click());
         onView(withId(R.id.more_espresso)).perform(click());
         onView(withId(R.id.chocolate)).perform(click());
         onView(withId(R.id.milk_type)).perform(click());
-        onData(anything()).atPosition(3).perform(click());
+        onData(anything()).atPosition(2).perform(click());
         onView(withText("Steamed")).perform(click());
+        onView(withId(R.id.custom_order_scrollview)).perform(swipeUp());
         onView(withId(R.id.review_order_button)).perform(click());
         onView(withId(R.id.name_text_box)).perform(typeText("Hassan"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.custom_order_name_box)).perform(typeText("MyFirstOrder"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.mail_order_button)).perform(click());
 
 
